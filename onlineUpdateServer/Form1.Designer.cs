6daa10f8b5faf43e776d63b3a78a79151f59a7ca
yueyue <1263375728@qq.com>
2017-02-15 21:39:53 +0800

    heart myself

--- onlineUpdateServer/Form1.Designer.cs
+++ onlineUpdateServer/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(401, 182);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(401, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "上传";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(128, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(294, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(550, 376);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "在线升级服务器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- onlineUpdateServer/Form1.cs
+++ onlineUpdateServer/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +15,85 @@ namespace onlineUpdateServer
 {
     public partial class Form1 : Form
     {
+        public class user
+        {
+            public Socket workSocket;
+            public static int bufferSize = 1024;
+            public byte[] buffer = new byte[bufferSize];
+        }
+        Socket serverSocket;
+        //客户端
+        List<user> clientSockets = new List<user>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.FileName != "")
+            {
+                if (this.openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    this.textBox1.Text = this.openFileDialog1.FileName;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EndPoint localEP = new IPEndPoint(IPAddress.Any, 5555);
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(localEP);
+            serverSocket.Listen(100);
+
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), serverSocket);
+        }
+        private void AcceptCallback(IAsyncResult ar)
+        {
+            Socket serverSocket = (Socket)ar.AsyncState;
+            Socket clientSocket = serverSocket.EndAccept(ar);
+
+            user client = new user();
+            client.workSocket = clientSocket;
+            //监听客户端加上去
+            clientSockets.Add(client);
+
+            clientSocket.BeginReceive(client.buffer,0,user.bufferSize, 0,
+                new AsyncCallback(ReceiveCallback),client);
+            serverSocket.BeginAccept(new AsyncCallback(AcceptCallback), serverSocket);
+        }
+        private void ReceiveCallback(IAsyncResult ar)
+        {
+            user client = (user)ar.AsyncState;
+            try
+            {
+                int receivelength = client.workSocket.EndReceive(ar);
+
+                string message = Encoding.Default.GetString(client.buffer, 0, receivelength);
+                if (message == "1111111111")
+                {
+                    Send(client);
+                }
+                client.workSocket.BeginReceive(client.buffer, 0, user.bufferSize, 0,
+                    new AsyncCallback(ReceiveCallback), client);
+            }
+            catch
+            {
+                MessageBox.Show("客户端已关闭");
+            }
+        }
+        private void Send(user client)
+        {
+            byte[] buffer = Encoding.Default.GetBytes("successful");
+            client.workSocket.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, new AsyncCallback(SendCallback),
+                     client);
+            MessageBox.Show("服务器发送1111成功");
+        }
+        private void SendCallback(IAsyncResult ar)
+        {
+            user SocketEnd = (user)ar.AsyncState;
+            int endlength = SocketEnd.workSocket.EndSend(ar);
         }
     }
 }
